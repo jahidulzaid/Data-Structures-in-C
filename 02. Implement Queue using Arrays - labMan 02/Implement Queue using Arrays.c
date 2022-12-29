37066d015fa15ef9cc8efbd8bf8df8b11c1c0620
Jahidul Zaid <jahidulzaid@gmail.com>
2022-12-29 21:53:58 +0600

    Added sol of lab 02

--- 02. Implement Queue using Arrays - labMan 02/Implement Queue using Arrays.c
+++ 02. Implement Queue using Arrays - labMan 02/Implement Queue using Arrays.c
@@ -6,23 +6,33 @@ int queue[ArraySize];
 int front = -1;
 int rear = -1;
 
-int isFull(int queue[ArraySize]){
+int isFull(int lastRearValue){
     if (rear == ArraySize -1)
         return 1;
     else
         return 0;
 }
 
-void isEmpty (int queue[ArraySize]){
-    
-}
 
 void enqueue(int itemToInsert){
-    if (isFull(rear)){
-        printf("\nOverflow.");
+    if (rear == ArraySize-1){
+        printf("\nOverflow");
     }
     else if (rear ==-1 && front == -1){
         front = rear = 0;
         queue[rear]= itemToInsert;
     }
+    else{
+        rear++;
+        queue[rear]= itemToInsert;
+    }
+}
+
+void display(int queue[ArraySize]){
+      
+}
+
+
+int main(){
+    enqueue(2);
 }
